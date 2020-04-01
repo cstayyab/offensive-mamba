68592246c64e8cd5b315f3b7de82ffb0b91c378f
Muhammad Tayyab Sheikh (cstayyab) <cstayyab@gmail.com>
2020-04-02 00:21:31 +0500

    Basic Client

--- localagent/ws_client.py
+++ localagent/ws_client.py
@@ -1,22 +1,47 @@
 import socketio
 import requests
+import json
+from getpass import getpass
+
+BASE_URL = "http://115.186.176.141:8080"
+HEADERS = {'Content-Type': 'application/json'}
 
 sio = socketio.Client()
 
 @sio.event
 def connect():
-    print('Connection established with Offensive Mamba Server')
+    print("[SUCCESS]")
 
 @sio.event
 def message(data):
-    print('message received with ', data)
-    sio.emit('message', {'response': 'my response'})
+    print(data)
+
+@sio.event
+def connection_failed(data):
+    print("[FAILED]")
 
 @sio.event
 def disconnect():
-    print('disconnected from server')
+    print('Disconnected from server')
+    exit(0)
 
+@sio.event
+def request(data):
+    req = json.loads(data)
+    print(req)
+    
+token = ""
+while True:
+    username = input("Enter Username: ")
+    password = getpass("Enter Password: ")
+    credentials = {"username": username, "password": password}
+    resp = requests.post(BASE_URL + "/login/", data=json.dumps(credentials), headers=HEADERS)
+    response = resp.json()
+    if(response['success']):
+        token = response['token']
+        break
+    print(response['message'])
 
-
-sio.connect('http://115.186.176.141:8080', headers={})
+print('Trying to connect . . . ', )
+sio.connect(BASE_URL, headers={"Authorization": "Bearer " + token})
 sio.wait()
