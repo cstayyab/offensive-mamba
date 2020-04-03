ac8ceecdc1ef18aa4260a573f60e6b928ccae18a
Muhammad Tayyab Sheikh (cstayyab) <cstayyab@gmail.com>
2020-04-03 14:01:10 +0500

    Packed Recievd Response

--- backend/api.py
+++ backend/api.py
@@ -424,7 +424,7 @@ class Msgrpc:
             #     self.login(self.msgrpc_user, self.msgrpc_pass)
             print(response)
             exit(1)
-        return bytes(response['resp'])
+        return msgpack.packb(response['resp'])
         # print(option)
         # params = msgpack.packb(option)
         # resp = ''
