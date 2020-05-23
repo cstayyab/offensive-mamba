e4ccd1e0548cec2276b90b658edf1bab065ba7d9
cstayyab <cstayyab@gmail.com>
2020-05-23 05:49:09 +0500

    CORS Working Test

--- backend/api.py
+++ backend/api.py
@@ -33,7 +33,7 @@ DBHANDLE = DatabaseHandler()
 connected_clients = {}
 all_requests = {}
 retrying_clients = []
-socketIOServer = socketio.Server(cors_allowed_origins='*', async_mode='threading')
+socketIOServer = socketio.Server(cors_allowed_origins=[], async_mode='threading')
 
 
 class BaseView(FlaskView):
